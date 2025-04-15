57c36270d06733f14494c77262090620f5444cb5
fogaoquatrobocas <133917201+fogaoquatrobocas@users.noreply.github.com>
2025-04-15 12:15:51 -0300

    ff

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-function toggleMateria(materia) {
-    const materias = ['biologia', 'historia', 'fisica'];
-    
-   
-    materias.forEach(m => {
-        document.getElementById(m).style.display = 'none';
-    });
-    
-    
+function showContent(materia) {
+    var contents = document.querySelectorAll('.content');
+    contents.forEach(content => content.style.display = 'none');
+
     document.getElementById(materia).style.display = 'block';
-}
-
-function showSubdivisao(subdivisaoId) {
-  
-    const subdivisoes = document.querySelectorAll('.subdivisao');
-    subdivisoes.forEach(sub => {
-        sub.classList.remove('active');
-    });
-
-  
-    document.getElementById(subdivisaoId).classList.add('active');
 }
